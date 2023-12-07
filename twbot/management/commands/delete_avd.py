c82685833f0519498612702527c51095e3964853
riken-khadela <rikenkhadelamain@gmail.com>
2023-12-07 16:48:41 +0530

    sss

--- twbot/management/commands/delete_avd.py
+++ twbot/management/commands/delete_avd.py
@@ -9,6 +9,8 @@ class Command(BaseCommand):
             
         inactive_user = User_details.objects.exclude(status='ACTIVE').order_by('?')
         all_inactive_avds = pd.read_csv('delete_avd.csv')
+        avd_list = subprocess.check_output(['emulator', '-list-avds'])
+        avd_list = [avd for avd in avd_list.decode().split("\n") if avd]
         list(User_details.objects.exclude(status='ACTIVE').order_by('?'))
         for in_user in inactive_user :
             avd = in_user.avdsname
@@ -34,15 +36,4 @@ class Command(BaseCommand):
                     print(e)
         
 
-        list(User_details.objects.exclude(status='ACTIVE').order_by('?'))
-        for in_user in inactive_user :
-            avd = in_user.avdsname
-            if avd in avd_list:
-                print(f"AVD {avd} with login issue so deleting")
-                try:
-                    subprocess.check_output(['avdmanager', 'delete', 'avd', '-n', avd])
-                    all_inactive_avds.drop(all_inactive_avds.index[all_inactive_avds['avd']== f'{avd}'], axis=0,inplace=True)
-                    all_inactive_avds.to_csv('delete_avd.csv',index=False)
-                except Exception as e:
-                    print(e)
-
+        
