cfaef3eddebe0c265c03eb739b6acb3fc1b53d3d
riken-khadela <rikenkhadelamain@gmail.com>
2023-12-15 20:07:51 +0530

    ss

--- twbot/management/commands/delete_avd.py
+++ twbot/management/commands/delete_avd.py
@@ -18,29 +18,25 @@ class Command(BaseCommand):
         avd_list = subprocess.check_output(['emulator', '-list-avds'])
         avd_list = [avd for avd in avd_list.decode().split("\n") if avd]
         
-        # all_inactive_avds = pd.read_csv('delete_avd.csv')
-        # print(len(all_inactive_avds))
-        # for avd in all_inactive_avds:
-        #     if avd in avd_list:
-        #         print(f"AVD {avd} with login issue so deleting")
-        #         try:
-        #             subprocess.check_output(['avdmanager', 'delete', 'avd', '-n', avd])
-        #             all_inactive_avds.drop(all_inactive_avds.index[all_inactive_avds['avd']== f'{avd}'], axis=0,inplace=True)
-        #             all_inactive_avds.to_csv('delete_avd.csv',index=False)
-        #         except Exception as e:
-        #             print(e)
+        all_inactive_avds = pd.read_csv('delete_avd.csv')
+        print(len(all_inactive_avds))
+        for avd in all_inactive_avds:
+            if avd in avd_list:
+                print(f"AVD {avd} with login issue so deleting")
+                try:
+                    subprocess.check_output(['avdmanager', 'delete', 'avd', '-n', avd])
+                    all_inactive_avds.drop(all_inactive_avds.index[all_inactive_avds['avd']== f'{avd}'], axis=0,inplace=True)
+                    all_inactive_avds.to_csv('delete_avd.csv',index=False)
+                except Exception as e:
+                    print(e)
         csv_path = os.path.join(os.getcwd(),'csv','this_pc_avd.csv')
         if os.path.exists(os.path.join(os.getcwd(),'csv','this_pc_avd.csv')) :
-            
-        
             this_pc_avds_list = pd.read_csv(csv_path)['Avdsname'].tolist()
-            
             for pcavd in avd_list :
                 if pcavd.startswith('instagram_') :
                     if not pcavd in this_pc_avds_list :
                         self.delete_avd(pcavd)
             
-            # return
             inactive_user = User_details.objects.exclude(status="ACTIVE")
             for user in inactive_user :
                 
