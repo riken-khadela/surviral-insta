9336b19f1612f10e7a101bf19ec399fb6143087b
riken-khadela <rikenkhadelamain@gmail.com>
2023-12-21 05:31:19 +0530

    ss

---
+++ tasks/auto_create.sh
@@ -0,0 +1,23 @@
+# project path
+export CURRENT_DIR=`dirname $(readlink -f $0)`
+export PRJ_DIR=`dirname $CURRENT_DIR`
+# go to project root directory
+cd $PRJ_DIR
+# . tasks/environment.sh
+
+# Kill python and AVD process
+killall -9 python qemu-system-x86_64
+
+# activate the virtual environment for python
+#. env/bin/activate
+. env/bin/activate
+
+# update code
+# git checkout old-insta-rk
+git stash
+git pull old-insta-rk
+
+# setup database
+python manage.py update_csv 
+python manage.py delete_avd
+python manage.py create_accounts -n=10000 
---
+++ tasks/auto_manage.sh
@@ -0,0 +1,24 @@
+# project path
+export CURRENT_DIR=`dirname $(readlink -f $0)`
+export PRJ_DIR=`dirname $CURRENT_DIR`
+# go to project root directory
+cd $PRJ_DIR
+#. ./tasks/environment.sh
+. tasks/environment.sh
+
+# Kill python and AVD process
+killall -9 python qemu-system-x86_64
+
+# activate the virtual environment for python
+#. env/bin/activate
+. env/bin/activate
+
+# update code
+git checkout old-insta-rk
+git stash
+git pull 
+
+# setup database
+python manage.py update_csv 
+python manage.py delete_avd 
+python manage.py on_pc --account_creation=True
