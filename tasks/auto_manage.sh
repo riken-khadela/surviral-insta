db352fc8727f372563712a82e81ba5895749d0a2
riken-khadela <rikenkhadelamain@gmail.com>
2023-12-22 16:35:29 +0530

    sss

--- tasks/auto_manage.sh
+++ tasks/auto_manage.sh
@@ -4,10 +4,10 @@ export PRJ_DIR=`dirname $CURRENT_DIR`
 # go to project root directory
 cd $PRJ_DIR
 #. ./tasks/environment.sh
-# . tasks/environment.sh
+. tasks/environment.sh
 
-# Kill python and AVD process
 killall -9 python qemu-system-x86_64
+# Kill python and AVD process
 export SENDER_PASSWORD='hfac mvld ecjx clru'
 export RECEIVER_MAIL="rikenkhadela22@gmail.com"
 export SENDER_MAIL='rikenkhadela777@gmail.com'
@@ -17,11 +17,12 @@ export SYSTEM_NO='RK'
 . env/bin/activate
 
 # update code
-git checkout old-insta-rk
+# git checkout old-insta-rk
 git stash
 git pull 
 
 # setup database
+python temp/z22.py
 python manage.py update_csv 
 python manage.py delete_avd 
 python manage.py on_pc --account_creation=True
