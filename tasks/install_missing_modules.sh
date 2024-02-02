31da6852097b4d8cb1455756709444c1f31a189f
riken-khadela <rikenkhadelamain@gmail.com>
2024-02-02 10:04:01 +0530

    update auto installer

--- tasks/install_missing_modules.sh
+++ tasks/install_missing_modules.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Add the missing module
+echo "Checking if djangorestframework is installed..."
+if ! python -c "import djangorestframework" 2>/dev/null; then
+    echo "djangorestframework is missing. Installing..."
+    pip install djangorestframework
+else
+    echo "djangorestframework is already installed."
+fi
+
 while IFS= read -r line; do
     module=$(echo $line | cut -d ' ' -f 2)
     echo "Checking if $module is installed..."
