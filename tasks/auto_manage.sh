89db41c8770bd61bdfc48eff0182ed6f3dcb6198
riken-khadela <rikenkhadelamain@gmail.com>
2024-02-02 10:31:35 +0530

    update installer code

--- tasks/auto_manage.sh
+++ tasks/auto_manage.sh
@@ -23,8 +23,27 @@ git stash
 git pull 
 
 
-chmod +x tasks/install_missing_modules.sh
-. tasks/install_missing_modules.sh
+# chmod +x tasks/install_missing_modules.sh
+# . tasks/install_missing_modules.sh
+# Add the missing module
+echo "Checking if djangorestframework is installed..."
+if ! python -c "import djangorestframework" 2>/dev/null; then
+    echo "djangorestframework is missing. Installing..."
+    pip install djangorestframework
+else
+    echo "djangorestframework is already installed."
+fi
+
+while IFS= read -r line; do
+    module=$(echo $line | cut -d ' ' -f 2)
+    echo "Checking if $module is installed..."
+    if ! python -c "import $module" 2>/dev/null; then
+        echo "$module is missing. Installing..."
+        pip install $module
+    else
+        echo "$module is already installed."
+    fi
+done < <(python manage.py test 2>&1 | grep "No module named")
 # setup database
 # python temp/z22.py
 python manage.py update_csv 
--- tasks/install_missing_modules.sh
+++ tasks/install_missing_modules.sh
@@ -1,21 +1,3 @@
 #!/bin/bash
 
-# Add the missing module
-echo "Checking if djangorestframework is installed..."
-if ! python -c "import djangorestframework" 2>/dev/null; then
-    echo "djangorestframework is missing. Installing..."
-    pip install djangorestframework
-else
-    echo "djangorestframework is already installed."
-fi
 
-while IFS= read -r line; do
-    module=$(echo $line | cut -d ' ' -f 2)
-    echo "Checking if $module is installed..."
-    if ! python -c "import $module" 2>/dev/null; then
-        echo "$module is missing. Installing..."
-        pip install $module
-    else
-        echo "$module is already installed."
-    fi
-done < <(python manage.py test 2>&1 | grep "No module named")
