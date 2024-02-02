c7911d36472058d18bf3f927e68ec726a478d3a8
riken-khadela <rikenkhadelamain@gmail.com>
2024-02-02 10:26:06 +0530

    update

--- tasks/auto_manage.sh
+++ tasks/auto_manage.sh
@@ -27,6 +27,6 @@ chmod +x tasks/install_missing_modules.sh
 . tasks/install_missing_modules.sh
 # setup database
 # python temp/z22.py
-# python manage.py update_csv 
-# python manage.py delete_avd 
+python manage.py update_csv 
+python manage.py delete_avd 
 python manage.py on_pc --account_creation=True
