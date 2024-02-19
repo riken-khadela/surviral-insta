19dbc403e4a2f0713aa7892471745cad0832c929
riken-khadela <rikenkhadelamain@gmail.com>
2024-02-19 16:25:58 +0530

    create command

--- twbot/management/commands/create_accounts.py
+++ twbot/management/commands/create_accounts.py
@@ -64,7 +64,7 @@ class Command(BaseCommand):
                     i.delete()
                     try:
                         subprocess.check_output(['avdmanager', 'delete', 'avd', '-n', avdname])
-                        LOGGER.info("Successfully deleted AVD:", avdname)
+                        LOGGER.info(f"Successfully deleted AVD: {avdname}")
                     except subprocess.CalledProcessError as e:
                         LOGGER.info(f"Error deleting AVD {avdname}: {e}")
                     except Exception as e:
