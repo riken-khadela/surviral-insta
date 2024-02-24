d8d6dc004548759886838893a31901bd0f39ca08
riken-khadela <rikenkhadelamain@gmail.com>
2024-02-24 14:41:47 +0530

    update code

--- twbot/middleware.py
+++ twbot/middleware.py
@@ -5,3 +5,6 @@ from twbot.bot import terminat_idel_connection
 class IdleConnectionMiddleware(MiddlewareMixin):
     def process_request(self, request):
         terminat_idel_connection()
+        if not os.path.isfile('.env'):
+            with open('.env', 'w'):pass
+            subprocess.run(['bash', 'task/variable.sh'])
